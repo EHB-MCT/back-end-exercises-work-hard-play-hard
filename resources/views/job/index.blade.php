dfb1d13f7decf1e17074faac9b9bc43420bb58a9
Bert Heyman <bert.heyman@hotmail.com>
2023-06-26 15:12:48 +0200

    Layout component

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,8 +1,14 @@
-<h1>Jobs</h1>
+<x-layout>
+    <x-slot:title>
+        Jobs
+    </x-slot:title>
 
-<h2>Daydreamer</h2>
-<p>Dream job, isn't it?</p>
+    <h1>Jobs</h1>
 
-<h2>Look busy advisor</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eum expedita sed. Aliquam blanditiis consequatur debitis eveniet maxime mollitia natus porro quod saepe sed soluta unde ut veniam veritatis, vero?</p>
+    <h2>Daydreamer</h2>
+    <p>Dream job, isn't it?</p>
+
+    <h2>Look busy advisor</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eum expedita sed. Aliquam blanditiis consequatur debitis eveniet maxime mollitia natus porro quod saepe sed soluta unde ut veniam veritatis, vero?</p>
+</x-layout>
 
