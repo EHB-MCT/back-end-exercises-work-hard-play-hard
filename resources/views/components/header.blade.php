e6c15bce37dc10b2c78da37d853f4c6d89bc71ce
Bert Heyman <bert.heyman@hotmail.com>
2023-06-26 15:28:36 +0200

    Header and footer

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('jobs.index') }}">Jobs</a></li>
+    </ul>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/jobs', [\App\Http\Controllers\JobController::class, 'index'])->name('jobs.index');
